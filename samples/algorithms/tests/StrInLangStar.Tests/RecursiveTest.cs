7f43a8a35c3847196f09b8b22257db1fbc0e547c
Zehua-Chen <peterchen06242000@outlook.com>
2020-03-05 22:46:03 -0600

    [algorithms] Add Tests

---
+++ samples/algorithms/tests/StrInLangStar.Tests/RecursiveTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using Algorithms;
+
+namespace StrInLangStar.Tests
+{
+    public class RecursiveTest
+    {
+        [Fact]
+        public void TestSimple()
+        {
+        }
+    }
+}
