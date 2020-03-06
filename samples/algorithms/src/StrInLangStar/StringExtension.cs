198d857ac49241ae1067c5e2ef53137c92b4202e
Zehua-Chen <peterchen06242000@outlook.com>
2020-03-05 22:40:19 -0600

    [algorithms] Setup C# Project

---
+++ samples/algorithms/src/StrInLangStar/StringExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public static class StringExtension
+    {
+        public static bool IsInLanguageStar(this string s, string language)
+        {
+            return false;
+        }
+    }
+}
